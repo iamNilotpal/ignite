9f6566e85a50da14c1da21981ce0b0f7289647f6
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2025-05-25 23:47:35 +0530

    feature/setup: Define configuration options for ignite.  (#4)
    - Add logger.
    - Define configuration options for ignite.
    - Use functional option pattern to set config values.

---
+++ pkg/options/defaults.go
@@ -0,0 +1,45 @@
+package options
+
+import "time"
+
+const (
+	// Specifies the default base directory where IgniteDB will store its data files.
+	// If no other directory is specified during initialization, this path will be used.
+	DefaultDataDir = "/ignitedb"
+
+	// Defines the default time duration between automatic compaction operations.
+	// By default, compaction will run every 5 hours.
+	DefaultCompactInterval = time.Hour * 5
+
+	// Represents the minimum allowed size for a segment file in bytes (512MB).
+	MinSegmentSize uint64 = 512 * 1024 * 1024
+
+	// Represents the maximum allowed size for a segment file in bytes (4GB).
+	MaxSegmentSize uint64 = 4 * 1024 * 1024 * 1024
+
+	// Specifies the default target size for a new segment file in bytes (1GB).
+	DefaultSegmentSize uint64 = 1 * 1024 * 1024 * 1024
+
+	// Specifies the default subdirectory within the main data directory
+	// where segment files will be stored.
+	DefaultSegmentDirectory = "/ignitedb/segments"
+
+	// Defines the default prefix for segment file names.
+	// For example, a segment file might be named "segment-00001.db".
+	DefaultSegmentPrefix = "segment"
+)
+
+// Holds the default configuration settings for an IgniteDB instance.
+var defaultOptions = Options{
+	DataDir:         DefaultDataDir,
+	CompactInterval: DefaultCompactInterval,
+	SegmentOptions: &segmentOptions{
+		Size:      DefaultSegmentSize,
+		Prefix:    DefaultSegmentPrefix,
+		Directory: DefaultSegmentDirectory,
+	},
+}
+
+func NewDefaultOptions() Options {
+	return defaultOptions
+}
